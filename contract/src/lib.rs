787abc13aaf60913f5ba4fba78017225ca0962cc
kyunghyunHan <ekfqlwcjswl@naver.com>
2023-04-10 10:48:19 +0900

    near_dapp

---
+++ contract/src/lib.rs
@@ -0,0 +1,79 @@
+// Find all our documentation at https://docs.near.org
+use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
+use near_sdk::{log, near_bindgen};
+
+#[near_bindgen]
+#[derive(Default,BorshDeserialize,BorshSerialize)]
+//counter 설정
+pub struct Counter{
+val:i8
+
+}
+
+#[near_bindgen]
+//read_only
+impl Counter{
+ pub fn get_num(&self)->i8{
+    return self.val;
+ }
+
+
+//카운터 증가
+ pub fn increment(&mut self){
+    self.val+=1;
+    log!("Increased number to {}", self.val);
+ }
+
+ pub fn decrement(&mut self){
+    self.val-=1;
+    log!("Decreased number to {}", self.val);
+ }
+
+   // reset
+   pub fn reset(&mut self) {
+    self.val = 0;
+    log!("Reset counter to zero");
+}
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn increment() {
+        // instantiate a contract variable with the counter at zero
+        let mut contract = Counter { val: 0 };
+        contract.increment();
+        assert_eq!(1, contract.get_num());
+    }
+
+    #[test]
+    fn decrement() {
+        let mut contract = Counter { val: 0 };
+        contract.decrement();
+        assert_eq!(-1, contract.get_num());
+    }
+
+    #[test]
+    fn increment_and_reset() {
+        let mut contract = Counter { val: 0 };
+        contract.increment();
+        contract.reset();
+        assert_eq!(0, contract.get_num());
+    }
+
+    #[test]
+    #[should_panic]
+    fn panics_on_overflow() {
+        let mut contract = Counter { val: 127 };
+        contract.increment();
+    }
+
+    #[test]
+    #[should_panic]
+    fn panics_on_underflow() {
+        let mut contract = Counter { val: -128 };
+        contract.decrement();
+    }
+}
